6fe04192d0801ad56d6c487eaef9fddaa49c5868
Maria Milagros Jaramillo Rios <mariwrios@gmail.com>
2021-08-12 01:01:16 -0400

    prueba de pn

---
+++ public/OneSignalSDKWorker.js
@@ -0,0 +1,9 @@
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+
+  window.OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "5ca34a3c-b092-4221-b45c-ab11a4fd7137",
+    });
+  });
